38bb4990dadbf728ef0e5a299330d42d8bab79c8
raj Thakur <rajthakur8827142011@gmail.com>
2024-01-12 23:23:26 +0530

    html node.js figlet 4 added

---
+++ NodeJS/Figlet/figlet4.js
@@ -0,0 +1,11 @@
+import figlet from "figlet";
+
+console.log(
+    figlet.textSync("THAKUR", {
+        font: "Ghost",
+        horizontalLayout: "default",
+        verticalLayout: "default",
+        width: 50,
+        whitespaceBreak: true,
+    })
+);
